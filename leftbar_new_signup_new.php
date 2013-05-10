f28084ec7ec279d7fa617e4bffc0f0152a478574
root <root@web1.talent.me>
2013-05-10 05:35:59 -0400

    test

---
+++ leftbar_new_signup_new.php
@@ -0,0 +1,22 @@
+
+	<div class="clearfix padL10">
+<div class="sig_sml">
+<div class="sig_sml_pic"><img src="<?=$PicPath?>" onerror="this.src='https://talent-small.s3.amazonaws.com/no-image-s.gif'" alt=""  /></div>
+<div style="min-height: 45px;" class="sig_sml_sig normalTxt"><?=$UserFullName?><br />
+<span class="normalTxt"><?=$HeadLine?></span><br />
+<span class="normalTxt"><?=$SubLine?></span>
+
+</div>
+</div>
+	   <div class="clearfix"></div></div>
+
+<div id="rec-left_menu" class="padT10">
+<div class="leftbold">
+
+</div>
+  <div class="padT10">
+<ul>
+<li><a href="JavaScript:void(0)" class="gyn">Grow Your Network</a></li>
+<li><a href="JavaScript:void(0)" class="gee">Get Endorsed</a></li>
+<li><a href="JavaScript:void(0)"  class="gie">Give Endorsement</a></li>
+</ul></div></div>
